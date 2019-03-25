eaa87b1594c79305ea55180a682404b8af343a22
donath-igaw <kimkyeseung@gmail.com>
2019-03-25 18:22:49 +0900

    [style] make img  align center of display component

--- src/container/display/index.vue
+++ src/container/display/index.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="display">
-    <h1>display</h1>
+    <img :src="uploadedImage" alt="user uploaded image">
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import ImageStore from '@/store/imageStoreModule';
 
 @Component
 export default class Display extends Vue {
-  test(ev) {
-    console.log('work', ev);
+  get uploadedImage() {
+    return ImageStore.image;
   }
 }
 
